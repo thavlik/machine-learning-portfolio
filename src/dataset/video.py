f27f9bd913d4fe98286316b3b982f5417644d463
Tom Havlik <keres993@gmail.com>
2020-11-22 09:57:22 -0600

    ...

--- src/dataset/video.py
+++ src/dataset/video.py
@@ -6,6 +6,7 @@ from math import floor
 import youtube_dl
 import cv2
 from torchvision.transforms import Resize, ToPILImage, ToTensor
+import json
 
 
 def load_links(path):
@@ -21,13 +22,18 @@ def load_links(path):
 
 def ensure_video_downloaded(id: str,
                             ext: str,
-                            cache_path: str):
+                            cache_path: str,
+                            download: bool):
     path = os.path.join(cache_path, f'{id}.{ext}')
     if os.path.exists(path):
         return path
+    elif not download:
+        raise ValueError(f'video file {path} not found.'
+                         'Hint: try again with download=True')
+    if not cache_path.endswith('/'):
+        cache_path += '/'
     with youtube_dl.YoutubeDL({
-        'outtmpl': '%(id)s.%(ext)s',
-        'cachedir': cache_path,
+        'outtmpl': cache_path + '%(id)s.%(ext)s',
     }) as ydl:
         video = ydl.extract_info(
             f'https://www.youtube.com/watch?v={id}',
@@ -44,25 +50,30 @@ def get_raw_frames(id: str,
                    ext: str,
                    start_frame: int,
                    num_frames: int,
-                   cache_path: str):
-    path = ensure_video_downloaded(id, ext, cache_path)
+                   skip_frames: int,
+                   cache_path: str,
+                   download: bool):
+    path = ensure_video_downloaded(id, ext, cache_path, download)
     cap = cv2.VideoCapture(path)
     if not cap.isOpened():
         raise ValueError(f"Error opening video stream or file")
-    frame_no = 0
-    end_frame = start_frame + num_frames
+    frames_per_example = num_frames * (skip_frames + 1) - skip_frames
+    end_frame = start_frame + frames_per_example
     frames = []
+    frame_no = 0  # global frame counter
+    idx = 0  # counter that starts at the right frame
     while cap.isOpened() and frame_no < end_frame:
         ret, frame = cap.read()
-        if ret == True:
-            if frame_no >= start_frame:
-                frames.append(frame)
-            frame_no += 1
-        else:
+        if ret != True:
             break
+        if frame_no >= start_frame:
+            if idx % (skip_frames + 1) == 0:
+                frames.append(frame)
+            idx += 1
+        frame_no += 1
     cap.release()
-    assert len(
-        frames) == num_frames, f"expect {num_frames} frames, got {len(frames)}"
+    if len(frames) != num_frames:
+        raise ValueError(f"expect {num_frames} frames, got {len(frames)}")
     return frames
 
 
@@ -80,10 +91,18 @@ def get_frames(id: str,
                ext: str,
                start_frame: int,
                num_frames: int,
+               skip_frames: int,
                width: int,
                height: int,
-               cache_path: str):
-    frames = get_raw_frames(id, ext, start_frame, num_frames, cache_path)
+               cache_path: str,
+               download: bool):
+    frames = get_raw_frames(id=id,
+                            ext=ext,
+                            start_frame=start_frame,
+                            num_frames=num_frames,
+                            skip_frames=skip_frames,
+                            cache_path=cache_path,
+                            download=download)
     frames = resize_frames(frames, width, height)
     return torch.stack(frames)
 
@@ -92,8 +111,9 @@ def get_all_frames(id: str,
                    ext: str,
                    width: int,
                    height: int,
-                   cache_path: str):
-    path = ensure_video_downloaded(id, ext, cache_path)
+                   cache_path: str,
+                   download: bool):
+    path = ensure_video_downloaded(id, ext, cache_path, download)
     cap = cv2.VideoCapture(path)
     if not cap.isOpened():
         raise ValueError(f"Error opening video stream or file")
@@ -110,10 +130,10 @@ def get_all_frames(id: str,
     return frames
 
 
-class DoomDataset(data.Dataset):
+class VideoDataset(data.Dataset):
     """
     Arguments:
-        path (str): path to links.txt
+        input_path (str): path to compiled.json (output from compiled.py)
 
         cache_path (str): video download cache path
 
@@ -125,64 +145,64 @@ class DoomDataset(data.Dataset):
         height (int): output resolution Y
 
         skip_frames (int): number of frames to skip between each
-        sampled frame
+        sampled frame. Default is 0, meaning no frames are skipped.
+
+        stride_frames (int): number of frames to offset between
+        examples. Default is 1, meaning no frames are skipped.
+
+        download (bool): allow downloading of missing videos
     """
 
     def __init__(self,
+                 input_path: str,
                  cache_path: str,
                  num_frames=1,
-                 width=640,
-                 height=480,
-                 skip_frames=0):
-
-        super(DoomDataset, self).__init__()
+                 width=320,
+                 height=240,
+                 skip_frames=0,
+                 stride_frames=1,
+                 download=False):
+        super(VideoDataset, self).__init__()
         self.width = width
         self.height = height
         self.num_frames = num_frames
         self.skip_frames = skip_frames
+        self.stride_frames = stride_frames
         self.cache_path = cache_path
-        source_videos = [{
-            'id': '4vkFdcbUtBU',
-            'ext': 'mp4',
-            'num_frames': 6000,
-        }, {
-            'id': '8FYSBH6K3xU',
-            'ext': 'mp4',
-            'num_frames': 6000,
-        }, {
-            'id': 'apo9Vb-5pWo',
-            'ext': 'mp4',
-            'num_frames': 9830,
-        }]
-        items = []
+        self.download = download
+        with open(input_path, 'r') as f:
+            self.videos = json.loads(f.read())
+        self.frames_per_example = num_frames * (skip_frames + 1) - skip_frames
         total_examples = 0
-        for video in source_videos:
-            video['num_examples'] = floor(
-                video['num_frames'] / (num_frames + skip_frames))
-            total_examples += video['num_examples']
-        self.source_videos = source_videos
-        self.items = items
+        for video in self.videos:
+            total_frames = video['duration'] * video['fps']
+            num_examples = total_frames - self.frames_per_example + 1
+            num_examples = floor(num_examples / stride_frames)
+            video['num_examples'] = num_examples
+            total_examples += num_examples
         self.total_examples = total_examples
 
     def __getitem__(self, index):
         cur = 0
-        for video in self.source_videos:
+        for video in self.videos:
             end = cur + video['num_examples']
             if index >= end:
                 cur = end
                 continue
             start_example = index - cur
-            start_frame = start_example * (self.num_frames + self.skip_frames)
-            if start_frame >= video['num_frames']:
+            start_frame = start_example * self.stride_frames
+            if start_frame + self.frames_per_example >= video['num_frames']:
                 raise ValueError(f"Unable to seek frame {start_frame} in video"
                                  f" with only {video['num_frames']} frames")
-            return get_frames(video['id'],
-                              video['ext'],
-                              start_frame,
-                              self.num_frames,
+            return get_frames(id=video['id'],
+                              ext=video['ext'],
+                              start_frame=start_frame,
+                              num_frames=self.num_frames,
+                              skip_frames=self.skip_frames,
                               width=self.width,
                               height=self.height,
-                              cache_path=self.cache_path)
+                              cache_path=self.cache_path,
+                              download=self.download)
         raise ValueError(
             f"unable to seek example, was dataset length calculated incorrectly?")
 
@@ -196,10 +216,11 @@ if __name__ == '__main__':
     width = 640
     height = 480
     num_frames = 9
-    ds = DoomDataset(cache_path='../dataset/cache',
-                     width=width,
-                     height=height,
-                     num_frames=num_frames)
+    ds = VideoDataset(input_path='../dataset/doom/compiled.json',
+                      cache_path='../dataset/doom/cache',
+                      width=width,
+                      height=height,
+                      num_frames=num_frames)
     ex = ds[0]
     assert ex.shape == (num_frames, 3, height, width)
 
