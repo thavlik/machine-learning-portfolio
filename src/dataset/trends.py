c59087f1cf6ebe208a9a644a26be2ce00134bbbf
Tom Havlik <keres993@gmail.com>
2020-11-26 11:17:14 -0600

    ...

--- src/dataset/trends.py
+++ src/dataset/trends.py
@@ -2,18 +2,26 @@ import os
 import numpy as np
 import torch
 import torch.utils.data as data
-
+import h5py
 
 class TReNDSDataset(data.Dataset):
-    def __init__(self):
+    def __init__(self, dir: str):
         super(TReNDSDataset, self).__init__()
+        self.dir = dir
+        self.files = os.listdir(dir)
 
     def __getitem__(self, index):
-        raise NotImplementedError
+        path = os.path.join(self.dir, self.files[index])
+        f = h5py.File(path, 'r')
+        data = f['SM_feature']
+        array = data[()]
+        raise array
 
     def __len__(self):
-        raise NotImplementedError
+        return len(self.files)
 
 
 if __name__ == '__main__':
-    pass
+    ds = TReNDSDataset('../../data/trends-fmri/fMRI_train')
+    print(ds[0].shape)
+    print(ds[1].shape)
