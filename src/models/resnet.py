a95ad457b6b897cb8a391c77a047c2d54657739d
Tom Havlik <keres993@gmail.com>
2020-11-26 12:54:36 -0600

    ...

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -6,7 +6,12 @@ import numpy as np
 
 
 def conv3x3(in_planes, out_planes, stride=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+    return nn.Conv2d(in_planes,
+                     out_planes,
+                     kernel_size=3,
+                     stride=stride,
+                     padding=1,
+                     bias=False)
 
 
 class BasicBlock(nn.Module):
@@ -19,18 +24,23 @@ class BasicBlock(nn.Module):
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = nn.BatchNorm2d(planes)
 
-        self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion * planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion * planes,
-                          kernel_size=1, stride=stride, bias=False),
+                nn.Conv2d(in_planes,
+                          self.expansion * planes,
+                          kernel_size=1,
+                          stride=stride,
+                          bias=False),
                 nn.BatchNorm2d(self.expansion * planes)
             )
+        else:
+            self.shortcut = None
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
+        if self.shortcut != None:
+            out += self.shortcut(x)
         out = F.relu(out)
         return out
 
@@ -40,22 +50,37 @@ class TransposeBasicBlock(nn.Module):
 
     def __init__(self, in_planes, planes, stride=1):
         super(TransposeBasicBlock, self).__init__()
-        self.conv1 = nn.ConvTranspose2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = nn.ConvTranspose2d(in_planes,
+                                        planes,
+                                        kernel_size=3,
+                                        stride=stride,
+                                        padding=1,
+                                        bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.ConvTranspose2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = nn.ConvTranspose2d(planes,
+                                        planes,
+                                        kernel_size=3,
+                                        stride=1,
+                                        padding=1,
+                                        bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
-        self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion * planes:
             self.shortcut = nn.Sequential(
-                nn.ConvTranspose2d(in_planes, self.expansion * planes,
-                                   kernel_size=1, stride=stride, bias=False),
+                nn.ConvTranspose2d(in_planes,
+                                   self.expansion * planes,
+                                   kernel_size=1,
+                                   stride=stride,
+                                   bias=False),
                 nn.BatchNorm2d(self.expansion * planes)
             )
+        else:
+            self.shortcut = None
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
+        if self.shortcut != None:
+            out += self.shortcut(x)
         out = F.relu(out)
         return out
