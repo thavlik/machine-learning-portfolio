f1e75da0de30f3a4eee42843946399505201d740
Tom Havlik <keres993@gmail.com>
2020-11-26 17:58:06 -0600

    ...

--- src/dataset/reference.py
+++ src/dataset/reference.py
@@ -1,13 +1,16 @@
 import torch.utils.data as data
 from torchvision import datasets
 
+
 class ReferenceDataset(data.Dataset):
-    """ Reference dataset loader that proxies a torchvision dataset
+    """ Reference dataset that proxies a torchvision stock dataset
     """
+
     def __init__(self,
-                 **params: dict):
+                 name: str,
+                 params: dict):
         super(ReferenceDataset, self).__init__()
-        self.ds = getattr(datasets, params['name'])(**params['params'])
+        self.ds = getattr(datasets, name)(**params)
 
     def __getitem__(self, index):
         return self.ds[index]
