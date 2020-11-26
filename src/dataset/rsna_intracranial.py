a52efe34abb85ab4aa08635303a06109fc1a793e
Tom Havlik <keres993@gmail.com>
2020-11-26 11:25:25 -0600

    ...

--- src/dataset/rsna_intracranial.py
+++ src/dataset/rsna_intracranial.py
@@ -2,26 +2,26 @@ import os
 import numpy as np
 import torch
 import torch.utils.data as data
-import h5py
+import pydicom
 
-class TReNDSDataset(data.Dataset):
+
+class RSNAIntracranialDataset(data.Dataset):
     def __init__(self, dir: str):
-        super(TReNDSDataset, self).__init__()
+        super(RSNAIntracranialDataset, self).__init__()
         self.dir = dir
         self.files = os.listdir(dir)
 
     def __getitem__(self, index):
         path = os.path.join(self.dir, self.files[index])
-        f = h5py.File(path, 'r')
-        data = f['SM_feature']
-        array = data[()]
-        return array
+        ds = pydicom.dcmread(path, stop_before_pixels=False)
+        data = ds.PixelData
+        return data
 
     def __len__(self):
         return len(self.files)
 
 
 if __name__ == '__main__':
-    ds = TReNDSDataset('../../data/trends-fmri/fMRI_train')
+    ds = RSNAIntracranialDataset('../../data/rsna-intracranial/stage_2_train')
     print(ds[0].shape)
     print(ds[1].shape)
