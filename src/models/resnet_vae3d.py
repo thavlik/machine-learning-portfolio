d793bff7f4e208eae984f9d9b7143a999d234d12
Tom Havlik <keres993@gmail.com>
2020-11-27 11:28:37 -0600

    ...

--- src/models/resnet_vae3d.py
+++ src/models/resnet_vae3d.py
@@ -33,14 +33,14 @@ class ResNetVAE3d(BaseVAE):
         in_features = channels
         for h_dim in hidden_dims:
             modules.append(BasicBlock3d(in_features, h_dim))
-            modules.append(nn.MaxPool3d((2, 1)))
+            modules.append(nn.MaxPool3d(2))
             in_features = h_dim
         self.encoder = nn.Sequential(
             *modules,
             nn.Flatten(),
             nn.Dropout(p=dropout),
         )
-        in_features = hidden_dims[-1] * width * height // 2**len(hidden_dims)
+        in_features = hidden_dims[-1] * width * height * depth // 2**len(hidden_dims)
         self.mu = nn.Sequential(
             nn.Linear(in_features, latent_dim),
             nn.BatchNorm1d(latent_dim),
@@ -63,7 +63,7 @@ class ResNetVAE3d(BaseVAE):
                 f'Unknown activation function "{output_activation}"')
 
         hidden_dims.reverse()
-        self.decoder_input = nn.Linear(latent_dim, hidden_dims[0] * 4)
+        self.decoder_input = nn.Linear(latent_dim, hidden_dims[0] * 8)
         modules = []
         in_features = hidden_dims[0]
         for h_dim in hidden_dims:
@@ -72,14 +72,14 @@ class ResNetVAE3d(BaseVAE):
         self.decoder = nn.Sequential(
             *modules,
             nn.Conv3d(hidden_dims[-1],
-                      width * height * channels // 4,
+                      width * height * depth * channels // 8,
                       kernel_size=3,
                       padding=1),
             act_options[output_activation](),
         )
 
     def encode(self, input: Tensor) -> List[Tensor]:
-        if input.shape[-3:] != (self.channels, self.height, self.width):
+        if input.shape[-4:] != (self.channels, self.depth, self.height, self.width):
             raise ValueError('wrong input shape')
         x = self.encoder(input)
         mu = self.mu(x)
@@ -88,30 +88,7 @@ class ResNetVAE3d(BaseVAE):
 
     def decode(self, z: Tensor) -> Tensor:
         x = self.decoder_input(z)
-        x = x.view(x.shape[0], self.hidden_dims[0], 2, 2)
+        x = x.view(x.shape[0], self.hidden_dims[0], 2, 2, 2)
         x = self.decoder(x)
-        x = x.view(x.shape[0], self.channels, self.height, self.width)
+        x = x.view(x.shape[0], self.channels, self.depth, self.height, self.width)
         return x
-
-    def loss_function(self,
-                      *args,
-                      **kwargs) -> dict:
-        result = super(ResNetVAE3d, self).loss_function(*args, **kwargs)
-
-        recons = args[0]
-        orig = args[1]
-
-        fid_weight = kwargs['fid_weight']
-        if fid_weight != 0.0:
-            fid_loss = self.fid(orig, recons)
-            result['FID_Loss'] = fid_loss
-            result['loss'] += fid_loss * fid_weight
-
-        return result
-
-    def fid(self, a: Tensor, b: Tensor) -> Tensor:
-        a = self.inception(a)
-        b = self.inception(b)
-        d = torch.sum([torch.mean((x - y) ** 2)
-                       for x, y in zip(a, b)])
-        return d
