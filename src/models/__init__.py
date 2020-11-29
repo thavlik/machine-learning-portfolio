e1b01f4e81ed257ca5a2712a969ed1a601e8a781
Tom Havlik <keres993@gmail.com>
2020-11-29 09:30:10 -0600

    ...

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,8 +1,4 @@
 from .base import BaseVAE
-from .resnet1d import BasicBlock1d, TransposeBasicBlock1d
-from .resnet2d import BasicBlock2d, TransposeBasicBlock2d
-from .resnet3d import BasicBlock3d, TransposeBasicBlock3d
-from .resnet4d import BasicBlock4d, TransposeBasicBlock4d
 from .resnet_vae1d import ResNetVAE1d
 from .resnet_vae2d import ResNetVAE2d
 from .resnet_vae3d import ResNetVAE3d
