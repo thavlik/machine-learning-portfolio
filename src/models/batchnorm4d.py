3e8cab7c9cbb35261e819780a685271e6453d777
Tom Havlik <keres993@gmail.com>
2020-11-29 09:35:12 -0600

    ...

--- src/models/batchnorm4d.py
+++ src/models/batchnorm4d.py
@@ -1,7 +1,7 @@
-from torch.nn import BatchNorm3d
+from torch.nn.modules.batchnorm import _BatchNorm
 
 
-class BatchNorm4d(BatchNorm3d):
+class BatchNorm4d(_BatchNorm):
     def _check_input_dim(self, input):
         if input.dim() != 6:
             raise ValueError('expected 6D input (got {}D input)'
