e77ac8dcb182f49af0a272a0af0cf0a2682a53fd
Tom Havlik <keres993@gmail.com>
2020-12-07 21:28:23 -0600

    ...

---
+++ src/render_teapot.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import matplotlib.pyplot as plt
+import torch
+import pytorch3d
+from pytorch3d.io import load_objs_as_meshes, load_obj
+from pytorch3d.structures import Meshes
+from pytorch3d.vis.plotly_vis import AxisArgs, plot_batch_individually, plot_scene
+from pytorch3d.vis.texture_vis import texturesuv_image_matplotlib
+from pytorch3d.renderer import (
+    look_at_view_transform,
+    FoVPerspectiveCameras, 
+    PointLights, 
+    DirectionalLights, 
+    Materials, 
+    RasterizationSettings, 
+    MeshRenderer, 
+    MeshRasterizer,  
+    SoftPhongShader,
+    TexturesUV,
+    TexturesVertex
+)
+
+device = torch.device("cpu")
+
+mesh = load_objs_as_meshes(['data/cow.obj'], device=device)
+
+
+# Initialize a camera.
+# With world coordinates +Y up, +X left and +Z in, the front of the cow is facing the -Z direction. 
+# So we move the camera by 180 in the azimuth direction so it is facing the front of the cow. 
+R, T = look_at_view_transform(2.7, 0, 180) 
+cameras = FoVPerspectiveCameras(device=device, R=R, T=T)
+
+# Define the settings for rasterization and shading. Here we set the output image to be of size
+# 512x512. As we are rendering images for visualization purposes only we will set faces_per_pixel=1
+# and blur_radius=0.0. We also set bin_size and max_faces_per_bin to None which ensure that 
+# the faster coarse-to-fine rasterization method is used. Refer to rasterize_meshes.py for 
+# explanations of these parameters. Refer to docs/notes/renderer.md for an explanation of 
+# the difference between naive and coarse-to-fine rasterization. 
+raster_settings = RasterizationSettings(
+    image_size=512, 
+    blur_radius=0.0, 
+    faces_per_pixel=1, 
+)
+
+# Place a point light in front of the object. As mentioned above, the front of the cow is facing the 
+# -z direction. 
+lights = PointLights(device=device, location=[[0.0, 0.0, -3.0]])
+
+# Create a phong renderer by composing a rasterizer and a shader. The textured phong shader will 
+# interpolate the texture uv coordinates for each vertex, sample from a texture image and 
+# apply the Phong lighting model
+renderer = MeshRenderer(
+    rasterizer=MeshRasterizer(
+        cameras=cameras, 
+        raster_settings=raster_settings
+    ),
+    shader=SoftPhongShader(
+        device=device, 
+        cameras=cameras,
+        lights=lights
+    )
+)
+images = renderer(mesh)
+plt.figure(figsize=(10, 10))
+plt.imshow(images[0, ..., :3].cpu().numpy())
+plt.grid("off")
+plt.axis("off")
+plt.show()
