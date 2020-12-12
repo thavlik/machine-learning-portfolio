a691f51d4388bf810669a413546818b0c7885287
Tom Havlik <keres993@gmail.com>
2020-12-12 01:05:06 -0600

    ...

--- src/vae_classifier.py
+++ src/vae_classifier.py
@@ -10,18 +10,18 @@ class VAEClassifierExperiment(VAEExperiment):
                  **kwargs) -> None:
         super().__init__(**kwargs)
         self.classifier = classifier
-    
+
     def training_step(self, batch, batch_idx, optimizer_idx=0):
         # First move to device so it doesn't happen twice
         real_img, labels = batch
         real_img = real_img.to(self.curr_device)
         batch = (real_img, labels)
         loss, results = super().training_step_raw(batch, batch_idx, optimizer_idx)
-        z = results[4] # Check BaseVAE.loss_function()
+        z = results[4]  # Check BaseVAE.loss_function()
         prediction = self.classifier(z)
         classifier_loss = self.classifier.loss_function(prediction, labels)
-        loss['loss'] += classifier_loss 
-        loss['Classifier_Loss'] = classifier_loss 
+        loss['loss'] += classifier_loss
+        loss['Classifier_Loss'] = classifier_loss
         return loss
 
     def validation_step(self, batch, batch_idx, optimizer_idx=0):
@@ -29,9 +29,9 @@ class VAEClassifierExperiment(VAEExperiment):
         real_img = real_img.to(self.curr_device)
         batch = (real_img, labels)
         loss, results = super().validation_step_raw(batch, batch_idx, optimizer_idx)
-        z = results[4] # Check BaseVAE.loss_function()
+        z = results[4]  # Check BaseVAE.loss_function()
         prediction = self.classifier(z)
         classifier_loss = self.classifier.loss_function(prediction, labels)
-        loss['loss'] += classifier_loss 
-        loss['Classifier_Loss'] = classifier_loss 
+        loss['loss'] += classifier_loss
+        loss['Classifier_Loss'] = classifier_loss
         return loss
