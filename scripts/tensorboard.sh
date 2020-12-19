1a1e4dbfe58391320f1c041359e7d21237a2eb12
Tom Havlik <keres993@gmail.com>
2020-12-19 16:30:01 -0600

    ...

--- scripts/tensorboard.sh
+++ scripts/tensorboard.sh
@@ -5,7 +5,7 @@ dir=$(pwd)
 docker run \
     -it \
     -p 6007:6006 \
-    -v $(wslpath -w logs)/EEGClassifBasic:/logs  \
+    -v $(wslpath -w logs)/DeepLesion_Basic:/logs  \
     tensorflow/tensorflow:latest-jupyter \
     tensorboard \
         --logdir /logs \
