74b6f6d7d669ceb45dfa8c33299d02aecf8987fa
Tom Havlik <keres993@gmail.com>
2020-12-18 23:51:26 -0600

    ...

--- src/entry.py
+++ src/entry.py
@@ -358,7 +358,7 @@ def experiment_main(config: dict, run_args: dict) -> pl.LightningModule:
     experiment = create_experiment(config, run_args)
     if experiment is None:
         return
-    #experiment = experiment.cuda()
+    experiment = experiment.cuda()
     tt_logger = TestTubeLogger(save_dir=run_args['save_dir'],
                                name=config['logging_params']['name'],
                                debug=False,
