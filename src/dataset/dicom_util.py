77ff53deaee6f58d893682262f43218b71b19e31
Tom Havlik <keres993@gmail.com>
2020-12-20 09:49:38 -0600

    ...

--- src/dataset/dicom_util.py
+++ src/dataset/dicom_util.py
@@ -34,7 +34,7 @@ def normalized_dicom_pixels(ds):
     x = x * slope + intercept
     x = torch.Tensor(x)
     if x.numel() != 512 * 512:
-        dim = torch.sqrt(x.numel().float())
+        dim = torch.sqrt(float(x.numel()))
         if dim.floor() != dim.ceil():
             raise ValueError('Non-square number of input elements '
                              f'got {x.numel()}')
