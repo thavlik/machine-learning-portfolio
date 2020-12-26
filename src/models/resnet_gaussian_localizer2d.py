ef5053b0f081ac25e6b4bd0ea5da56c95b3ab5ba
Tom Havlik <keres993@gmail.com>
2020-12-26 08:59:43 -0600

    ...

--- src/models/resnet_gaussian_localizer2d.py
+++ src/models/resnet_gaussian_localizer2d.py
@@ -4,12 +4,13 @@ from torch.nn import functional as F
 from .classifier import Classifier
 from .resnet2d import BasicBlock2d, TransposeBasicBlock2d
 from torch import nn, Tensor
+from torch.distributions import Normal
 from abc import abstractmethod
 from typing import List, Callable, Union, Any, TypeVar, Tuple
 from math import sqrt, ceil
 from .inception import InceptionV3
 from .localizer import Localizer
-from .util import get_pooling2d, get_activation, reparameterize
+from .util import get_pooling2d, get_activation
 from .resnet_localizer2d import ResNetLocalizer2d
 
 
@@ -19,16 +20,19 @@ class ResNetGaussianLocalizer2d(ResNetLocalizer2d):
                  **kwargs) -> None:
         super().__init__(**kwargs)
         self.kappa = kappa
-        log_var = [nn.Linear(self.prediction.in_features, 4)]
+        std_dev = [nn.Linear(self.prediction.in_features, 4)]
         if self.batch_normalize:
-            log_var.append(nn.BatchNorm1d(4))
-        log_var.append(self.activation)
-        self.log_var = nn.Sequential(*log_var)
+            std_dev.append(nn.BatchNorm1d(4))
+        std_dev.append(self.activation)
+        self.std_dev = nn.Sequential(*std_dev)
 
     def forward(self, x: Tensor) -> Tensor:
         y = self.layers(x)
         mu = self.output(self.prediction(y))
-        log_var = self.log_var(y)
-        pred = reparameterize(mu, log_var)
-        pred = torch.lerp(mu, pred, self.kappa)
+        std_dev = self.std_dev(y)
+        pred = reparameterize_normal(mu, std_dev * self.kappa)
         return pred
+
+
+def reparameterize_normal(mu: Tensor, std_dev: Tensor) -> Tensor:
+    return Normal(mu, std_dev).rsample(mu.shape)
