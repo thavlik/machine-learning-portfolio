bc84a93ce25e38dcd228da1cae57b9f51cf6ef11
Tom Havlik <keres993@gmail.com>
2021-03-02 23:48:03 -0600

    ...

--- src/dataset/forrestgump.py
+++ src/dataset/forrestgump.py
@@ -14,66 +14,6 @@ from torch import Tensor
 from typing import Optional
 
 
-def convert_forrest_gump_h5py(root: str,
-                              alignment: Optional[str] = None,
-                              max_chunk_samples: int = 16):
-    if alignment is None:
-        data_dir = root
-        identifier = 'acq-raw'
-    elif alignment == 'linear':
-        data_dir = os.path.join(root, 'linear_anatomical_alignment')
-        identifier = 'rec-dico7Tad2grpbold7Tad'
-    elif alignment == 'nonlinear':
-        data_dir = os.path.join(
-            root, 'non-linear_anatomical_alignment')
-        identifier = 'rec-dico7Tad2grpbold7TadNL'
-    else:
-        raise ValueError(f"unknown alignment value '{alignment}'")
-    subjects = [f for f in os.listdir(data_dir)
-                if f.startswith('sub-') and len(f) == 6 and int(f[len('sub-'):]) <= 20]
-    num_frames = 3599
-    out_dir = os.path.join(root, 'converted', identifier)
-    try:
-        os.mkdir(out_dir)
-    except:
-        pass
-    metadata = {}
-    for subject in subjects:
-        print(f'Converting {subject}')
-        try:
-            os.mkdir(os.path.join(out_dir, subject))
-        except:
-            pass
-        subj_no = int(subject[4:])-1
-        frame_no = 0
-        frames = None
-        for run in range(8):
-            filename = f'{subject}_ses-forrestgump_task-forrestgump_{identifier}_run-0{run+1}_bold.nii.gz'
-            filename = os.path.join(
-                data_dir, subject, 'ses-forrestgump', 'func', filename)
-            img = nl.image.load_img(filename)
-            img = img.get_data()
-            img = np.transpose(img, (3, 2, 0, 1))
-            frames = img if frames is None else np.concatenate(
-                (frames, img), axis=0)
-        if frames.shape[0] != num_frames:
-            print(f'WARNING: {subject} has {len(frames)} frames, expected {num_frames}')
-        metadata[subject] = {
-            'num_frames': frames.shape[0],
-        }
-        with open(os.path.join(out_dir, 'metadata.json'), 'w') as f:
-            f.write(json.dumps(metadata))
-        num_chunks = ceil(frames.shape[0] / max_chunk_samples)
-        for i in range(num_chunks):
-            a = max_chunk_samples * i
-            b = min(a + max_chunk_samples, frames.shape[0])
-            out_path = os.path.join(out_dir, subject, subject + f'_{i}')
-            chunk = frames[a:b, ...]
-            np.save(out_path, chunk)
-    with open(os.path.join(out_dir, 'metadata.json'), 'w') as f:
-        f.write(json.dumps(metadata))
-
-
 def load_scenes(path: str) -> list:
     with open(path, "r") as f:
         result = []
@@ -81,28 +21,53 @@ def load_scenes(path: str) -> list:
             parts = line.strip().split(',')
             t = float(parts[0])
             name = parts[1][1:-1]
-            is_day = parts[2] == "\"DAY\""
-            is_exterior = parts[3] == "\"INT\""
+            is_day = 1 if parts[2] == "\"DAY\"" else 0
+            is_exterior = 1 if parts[3] == "\"INT\"" else 0
             result.append((t, name, is_day, is_exterior))
         return result
 
 
-def calc_scene_examples(scenes: list,
-                        num_frames: int):
-    frame_dur_sec = 2.0
-    scene_examples = []
-    for i in range(len(scenes)):
-        t0 = scenes[i+0][0]
-        if i == len(scenes) - 1:
-            t1 = 3599.0 * frame_dur_sec
+def soft_label(scenes: list, t0: float, t1: float) -> float:
+    """ Calculate soft labels, defined as the weighted average
+    of labels across all involved scenes.
+    """
+    if t1 < scenes[0][0]:
+        return (0, 0)
+    values = []
+    weights = []
+    for i, scene in enumerate(scenes):
+        if scene[0] > t1:
+            break
+        s0 = scene[0]
+        if i == len(scenes)-1:
+            s1 = 7198.0
         else:
-            t1 = scenes[i+1][0]
-        dur_sec = t1 - t0
-        num_examples = dur_sec / frame_dur_sec - num_frames + 1
-        num_examples = floor(num_examples)
-        num_examples = max(0, num_examples)
-        scene_examples.append(num_examples)
-    return scene_examples
+            s1 = scenes[i+1][0]
+        # t0 within s, t1 within s, s completely within t, t completely within s
+        if (s0 <= t0 and t0 <= s1) or (s0 <= t1 and t1 <= s1) or (t0 <= s0 and s1 <= t1) or (s0 <= t0 and t1 <= s1):
+            i0 = max(s0, t0)
+            i1 = min(s1, t1)
+            dur = i1 - i0
+            weights.append(dur)
+            labels = scene[2:]
+            values.append(labels)
+    labels = np.average(values, axis=0, weights=weights)
+    return labels
+
+
+def convert_labels(scenes: list, frame_dur: float) -> list:
+    labels = []
+    for i in range(3599):
+        t0 = i * frame_dur
+        t1 = t0 + frame_dur
+        label = soft_label(scenes, t0, t1)
+        labels.append(label)
+    return np.array(labels)
+
+
+def load_metadata(path: str):
+    with open(path, "r") as f:
+        return json.loads(f.read())
 
 
 class ForrestGumpDataset(data.Dataset):
@@ -126,7 +91,7 @@ class ForrestGumpDataset(data.Dataset):
             be necessary.
 
         alignment: Optional alignment transformation geometry. Valid values
-            are "linear" and "nonlinear".
+            are "raw", "linear", and "nonlinear".
 
     Labels:
         0: The scene takes place indoors
@@ -144,13 +109,11 @@ class ForrestGumpDataset(data.Dataset):
         https://www.math.mcgill.ca/keith/delay/delay.pdf.
     """
 
-    FILE_DURATIONS = [902, 882, 876, 976, 924, 878, 1084, 676]
-
     def __init__(self,
                  root: str,
                  num_frames: int = 8,
                  offset_frames: int = 0,
-                 alignment: Optional[str] = None):
+                 alignment: Optional[str] = 'raw'):
         super(ForrestGumpDataset, self).__init__()
         if offset_frames != 0:
             raise NotImplementedError
@@ -158,108 +121,35 @@ class ForrestGumpDataset(data.Dataset):
         self.num_frames = num_frames
         self.scenes = load_scenes(os.path.join(
             root, "stimuli", "annotations", "scenes.csv"))
-        if alignment is None:
-            self.data_dir = root
-            self.identifier = 'acq-raw'
+        self.labels = convert_labels(self.scenes, frame_dur=2.0)
+        if alignment == 'raw':
+            self.data_dir = os.path.join(root, 'converted', 'raw')
         elif alignment == 'linear':
-            self.data_dir = os.path.join(
-                root, 'derivatives', 'linear_anatomical_alignment')
-            self.identifier = 'rec-dico7Tad2grpbold7Tad'
+            self.data_dir = os.path.join(root, 'converted', 'linear')
         elif alignment == 'nonlinear':
-            self.data_dir = os.path.join(
-                root, 'derivatives', 'non-linear_anatomical_alignment')
-            self.identifier = 'rec-dico7Tad2grpbold7TadNL'
+            self.data_dir = os.path.join(root, 'converted', 'nonlinear')
         else:
             raise ValueError(f"unknown alignment value '{alignment}'")
-        subjects = [f for f in os.listdir(self.data_dir)
-                    if f.startswith('sub-') and len(f) == 6 and int(f[len('sub-'):]) <= 20]
-        self.subjects = subjects
-        self.scene_examples = calc_scene_examples(self.scenes,
-                                                  num_frames=num_frames)
-        self.examples_per_subject = sum(self.scene_examples)
+        metadata = load_metadata(os.path.join(self.data_dir, 'metadata.json'))
+        self.subjects = metadata['subjects']
+        self.subject_keys = sorted(self.subjects.keys())
 
     def __getitem__(self, index):
-        frame_dur_sec = 2.0
-        example_dur = frame_dur_sec * self.num_frames
-        subj_no = int(floor(index / self.examples_per_subject))
-        subj = self.subjects[subj_no]
-        example_no = index % self.examples_per_subject
-        scene_no = 0
+        sub_no = 0
         offset = 0
-        for scene, num_examples in zip(self.scenes, self.scene_examples):
-            if offset + num_examples > example_no:
+        for subject in self.subjects:
+            num_frames = subject['num_frames']
+            if offset + num_frames > index:
                 break
-            offset += num_examples
-            scene_no += 1
-        scene_example = example_no - offset
-        scene = self.scenes[scene_no]
-        label = 1 if scene[3] else 0
-        start_time = scene[0] + frame_dur_sec * scene_example
-        end_time = start_time + example_dur
-        start_file = None
-        end_file = None
-        file_start_time = 0
-        for i, file_time in enumerate(self.FILE_DURATIONS):
-            file_end_time = file_start_time + file_time
-            if file_start_time <= start_time and start_time < file_end_time:
-                start_file = i+1
-            if file_start_time < end_time and end_time <= file_end_time:
-                end_file = i+1
-            if start_file is not None and end_file is not None:
-                break
-            file_start_time = file_end_time
-        if start_file is None:
-            raise ValueError("unable to seek start file")
-        if end_file is None:
-            raise ValueError("unable to seek end file")
-        if start_file != end_file:
-            start_img_time = sum(self.FILE_DURATIONS[:start_file])
-            start_dur = start_img_time - start_time
-            start_frames = int(start_dur / frame_dur_sec)
-            remainder = int(self.num_frames - start_frames)
-
-            start_path = f'{subj}_ses-forrestgump_task-forrestgump_{self.identifier}_run-0{start_file}_bold.nii.gz'
-            start_path = os.path.join(self.data_dir, subj,
-                                      'ses-forrestgump', 'func', start_path)
-            start_img = nl.image.load_img(start_path)
-            start_img = start_img.get_data()
-            start_img = start_img[:, :, :, start_img.shape[-1]-start_frames:]
-            start_img = np.transpose(start_img, (3, 2, 0, 1))
-
-            end_path = f'{subj}_ses-forrestgump_task-forrestgump_{self.identifier}_run-0{end_file}_bold.nii.gz'
-            end_path = os.path.join(self.data_dir, subj,
-                                    'ses-forrestgump', 'func', end_path)
-            end_img = nl.image.load_img(end_path)
-            end_img = end_img.get_data()
-            end_img = end_img[:, :, :, :remainder]
-            end_img = np.transpose(end_img, (3, 2, 0, 1))
-
-            img = np.concatenate([start_img, end_img], axis=0)
-        else:
-            filename = f'{subj}_ses-forrestgump_task-forrestgump_{self.identifier}_run-0{start_file}_bold.nii.gz'
-            filename = os.path.join(self.data_dir, subj,
-                                    'ses-forrestgump', 'func', filename)
-            img = nl.image.load_img(filename)
-            img = img.get_data()
-            img = img[:, :, :, scene_example:scene_example+self.num_frames]
-            img = np.transpose(img, (3, 2, 0, 1))
-        return (img, label)
+            offset += num_frames
+            sub_no += 1
+        raise NotImplementedError
 
     def __len__(self):
-        return self.examples_per_subject * len(self.subjects)
+        raise NotImplementedError
 
 
 if __name__ == '__main__':
-    convert_forrest_gump_h5py('/data/openneuro/ds000113-download')
-    ds = ForrestGumpDataset(
-        root='/data/openneuro/ds000113-download', alignment='linear')
-    #print(f'last: {ds[len(ds)-1][1]}')
-    for i in range(len(ds.subjects)):
-        print(
-            f'subject {i+1}: {ds[ds.examples_per_subject * i][1]}, {ds[ds.examples_per_subject * (i+1) - 1][1]}')
-    # for i, x in enumerate(ds):
-    #    print(f'{i}. {x[1]}')
-    #i = len(ds) - 1
-    # while i >= 0:
-    #    print(f'{i}. {ds[i][1]}')
-    #    i -= 1
+    ds = ForrestGumpDataset(root='/data/openneuro/ds000113-download',
+                            alignment='linear')
+    print(ds[0])
